197f1d6a855e1fe49f612bf473cb4af9e1564521
Luke Batchelor <lbatchelor@atlassian.com>
2018-08-20 19:22:12 +1000

    Fixes bug in DOM

--- scripts/build-binary-decoder.js
+++ scripts/build-binary-decoder.js
@@ -78,8 +78,8 @@ function buildDecoderHtml(maxDepth) {
         <label for="start" id="resetButton">Reset</label>
       </div>
     </div>
-    <footer class="footer">Check out <a href="https://github.com/lukebatchelor/css-binary-decoder">lukebatchelor/css-binary-decoder</a> if you're interested in how it works!</p>
-    <a href="https://github.com/lukebatchelor/css-binary-decoder"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></footer>
+    <footer class="footer">Check out <a href="https://github.com/lukebatchelor/css-binary-decoder">lukebatchelor/css-binary-decoder</a> if you're interested in how it works!</footer>
+    <a href="https://github.com/lukebatchelor/css-binary-decoder"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub">
     </body>
     </html>
   `;
